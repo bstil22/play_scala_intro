3216f39058bc90a188669943902bdfc3f27965f1
bstil22 <brady.stilwell@csaa.com>
2016-01-26 00:53:10 -0700

    add a task

--- app/controllers/Tasks.scala
+++ app/controllers/Tasks.scala
@@ -2,11 +2,18 @@ package controllers
 
 import play.api._
 import play.api.mvc._
+import models.Task
 
 class Tasks extends Controller {
 
   def index = Action {
-    Ok(views.html.tasks.index("Add a task"))
+    Ok(views.html.tasks.index(Task.all))
+  }
+
+  def addTask = Action(parse.urlFormEncoded){
+    implicit request =>
+      Task.add(request.body.get("taskName").get.head)
+      Redirect(routes.Tasks.index)
   }
 
 }
